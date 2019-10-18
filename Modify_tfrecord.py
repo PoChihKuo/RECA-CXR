13082725f9eb6f060cf34617ad27128e95c148d8
PoChihKuo <bfantasykuo@gmail.com>
2019-10-18 14:09:50 -0400

    Update Modify_tfrecord.py

--- Modify_tfrecord.py
+++ Modify_tfrecord.py
@@ -9,17 +9,18 @@ with tf.io.TFRecordWriter(record_file) as writer:
         example.ParseFromString(string_record)
         image_feature=example.features.feature['jpg_bytes'].bytes_list.value[0]
 
+        # from Byte to Img
         imageStream = io.BytesIO(image_feature)
         imageOrigin = Image.open(imageStream)
         
         #Replace your function here
         image_moire=moire_pattern(imageOrigin)
         
+        # from Img to Byte
         imgByteArr = io.BytesIO()
         image_moire.save(imgByteArr, format='JPEG')
         imgByteArr = imgByteArr.getvalue()       
         
-        example.features.feature['jpg_bytes'].bytes_list.value[0]=imgByteArr
-        
-        patient_num=example.features.feature['patient'].int64_list.value[0]
+        # Put it back to tf_record
+        example.features.feature['jpg_bytes'].bytes_list.value[0]=imgByteArr        
         writer.write(example.SerializeToString())
